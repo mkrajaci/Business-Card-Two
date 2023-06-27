f20fd543ab4b56c75f753a6c877d1290b3faa786
Mario Krajacic <krajacic.mario@gmail.com>
2023-06-27 16:20:35 +0200

    Initial Commit

---
+++ Business Card Two/Business_Card_TwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Business_Card_TwoApp.swift
+//  Business Card Two
+//
+//  Created by Mario Krajačić on 27.06.2023..
+//
+
+import SwiftUI
+
+@main
+struct Business_Card_TwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
